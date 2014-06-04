8317dc7903bd92c372ebf6fb75c42c9aa103bb02
Eric J. Humphrey <ejhumphrey@nyu.edu>
2014-06-04 17:57:54 +0200

    Made fit_transform a bit more flexible, no idea if it works tho

--- matlab/fit_transform.sh
+++ matlab/fit_transform.sh
@@ -1,29 +1,53 @@
 #!/bin/bash
 
-FEATURES=tmc_features
-
-# FOLD_IDXS=$(seq 0 4)
-FOLD_IDXS="0"
 BASEDIR=/Users/ejhumphrey
 METADATA=${BASEDIR}/metadata
-FEATURE_DIR=${BASEDIR}/${FEATURES}
 
-ESTIMATIONS=${BASEDIR}/estimations/${FEATURES}
-MODELS=${BASEDIR}/models/${FEATURES}
-for idx in ${FOLD_IDXS}
+if [ -z "$1" ]; then
+    echo "Usage:"
+    echo "fit_transform.sh {features|all} {[0-4]|*all}"
+    echo $'\tfeatures - Name of the feature representation.'
+    echo $'\tfold# - Number of the training fold, default=all.'
+    exit 0
+fi
+
+if [ "$1" == "all" ]
+then
+    echo "Setting all known features..."
+    FEATURES="tmc_features"
+else
+    FEATURES="$1"
+fi
+
+if [ "$2" == "all" ] || [ -z "$2" ];
+then
+    echo "Setting all folds"
+    FOLD_IDXS=$(seq 0 4)
+else
+    FOLD_IDXS=$2
+fi
+
+for features in ${FEATURES}
 do
-    MODELDIR=${MODELS}/${idx}
-    TRAINLIST=${METADATA}/train${idx}.txt
-    matlab -nodisplay -nosplash -r "extractFeaturesAndTrain "\
+    FEATURE_DIR=${BASEDIR}/${features}
+
+    ESTIMATIONS=${BASEDIR}/estimations/${features}
+    MODELS=${BASEDIR}/models/${features}
+    for idx in ${FOLD_IDXS}
+    do
+        MODELDIR=${MODELS}/${idx}
+        TRAINLIST=${METADATA}/train${idx}.txt
+        matlab -nodisplay -nosplash -r "extractFeaturesAndTrain "\
 "${TRAINLIST} "\
 "${FEATURE_DIR} "\
 "${MODELDIR};exit"
 
-    TESTLIST=${METADATA}/test${idx}.txt
-    OUTPUTDIR=${ESTIMATIONS}/${idx}
-    matlab -nodisplay -nosplash -r "doChordID "\
+        TESTLIST=${METADATA}/test${idx}.txt
+        OUTPUTDIR=${ESTIMATIONS}/${idx}
+        matlab -nodisplay -nosplash -r "doChordID "\
 "${TESTLIST} "\
 "${FEATURE_DIR} "\
 "${MODELDIR} "\
 "${OUTPUTDIR};exit"
+    done
 done
